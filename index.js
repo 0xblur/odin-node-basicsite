a1dd8859b2ccc87c7a3e20587c2a24641fb6e979
cypherHades <xjadezbix@gmx.com>
2024-04-14 09:34:17 -0500

    refactor: from plain node to express app

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
-const http = require("node:http");
-const fs = require("node:fs/promises");
+import express from "express";
+import createError from "http-errors";
+import path from "node:path";
 
-const server = http.createServer((req, res) => {
-	if (req.url === "/") {
-		const serveIndex = async () => {
-			const page = await fs.readFile("./index.html", { encoding: "utf8" });
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.write(page);
-			res.end();
-		};
-		serveIndex();
-	} else if (req.url === "/about") {
-		const serveAbout = async () => {
-			const page = await fs.readFile("./about.html", { encoding: "utf8" });
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.write(page);
-			res.end();
-		};
-		serveAbout();
-	} else if (req.url === "/contact-me") {
-		const serveContactMe = async () => {
-			const page = await fs.readFile("./contact-me.html", { encoding: "utf8" });
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.write(page);
-			res.end();
-		};
-		serveContactMe();
-	} else {
-		const serveErrorPage = async () => {
-			const page = await fs.readFile("./404.html", { encoding: "utf8" });
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.write(page);
-			res.end();
-		};
-		serveErrorPage();
-	}
+const app = express();
+app.get("/", (req, res, next) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/about", (req, res, next) => {
+	res.sendFile(path.join(__dirname, "about.html"));
+});
+app.get("/contact-me", (req, res, next) => {
+	res.sendFile(path.join(__dirname, "contact-me.html"));
 });
 
-server.listen(8000);
+app.all("*", (req, res, next) => {
+	res.status(404).send("<h1>404: Not found!</h1>");
+	next(createError("404"));
+});
+
+app.listen(8000);
